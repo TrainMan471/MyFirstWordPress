f5e084074100cab3ee278497d653db7069ee9a9a
Michael Brown <Michael Brown>
2016-09-08 21:07:36 -0400

    Uploading start of custom theme

---
+++ wordpress/wordpress/wp-content/themes/myfirsttheme/index.php
@@ -0,0 +1,17 @@
+
+<!DOCTYPE html>
+<html>
+
+<head>
+  <meta charset="utf-8">
+  <title></title>
+  <meta name="author" content="">
+  <meta name="description" content="">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+
+<body>
+  <p>Hello, World! The year is <?php echo date(Y);?>.</p>
+
+</body>
+</html>
